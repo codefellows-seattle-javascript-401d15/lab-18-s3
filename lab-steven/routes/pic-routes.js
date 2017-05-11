46cf43b0bcd1337fd5f3e8fbc1e3142ca2a84f98
Steven Johnson <stevenjohnson86@gmail.com>
2017-05-11 12:45:14 -0700

    added pic modules, manual testing failing

--- lab-steven/routes/pic-routes.js
+++ lab-steven/routes/pic-routes.js
@@ -2,29 +2,11 @@
 
 const debug = require('debug')('cfgram: picRoutes');
 const bearerAuth = require('../lib/bearer-auth-middleware');
-const fs = require('fs');
-const path = require('path');
-const del = require('del');
-const AWS  = require('aws-sdk');
 const multer = require('multer');
-const dataDir = `${__dirname}/../data`
+const dataDir = `${__dirname}/../data`;
 const upload = multer({dest: dataDir});
-const createError = require('http-errors');
 
-const Pic = require('../model/pic');
-const Gallery = require('../model/gallery');
-
-AWS.config.setPromisesDependency(require('bluebird'));
-
-const s3 = new AWS.S3();
-
-function s3UploadProm(params){
-  return new Promise((resolve, reject) => {
-    s3.upload(params, (err, data) => {
-      resolve(data);
-    });
-  });
-}
+const picCtrlr = require('../controller/pic-controller');
 
 module.exports = function(router){
   debug('#pic-routes');
@@ -32,39 +14,26 @@ module.exports = function(router){
   router.post('/gallery/:id/pic', bearerAuth, upload.single('image'), (req, res) => {
     debug('#POST gallery/:id/pic');
 
-    if(!req.file) return createError(400, 'Resource required');
-    if(!req.file.path) return createError(500, 'File not saved');
-
-    let ext = path.extname(req.file.originalname);
-    let params = {
-      ACL: 'public-read',
-      Bucket: process.env.AWS_BUCKET,
-      Key: `${req.file.filename}${ext}`,
-      Body: fs.createReadStream(req.file.path),
-    };
-
-    return Gallery.findById(req.params.id)
-    .then(() => s3UploadProm(params))
-    .then(s3Data => {
-      del([`${dataDir}/*`]);
-      let picData = {
-        name: req.body.name,
-        desc: req.body.desc,
-        userID: req.user._id,
-        galleryID: req.params.id,
-        imageURI: s3Data.Location,
-        objectKey: s3Data.Key,
-      };
-      return new Pic(picData).save();
-    })
+    return picCtrlr.createPic(req)
     .then(pic => res.json(pic))
     .catch(err => res.status(err.status).send(err.message));
   });
 
-  router.get('/pic/:id', (req, res) => {
+  router.get('/pic/:id', bearerAuth, (req, res) => {
     debug('#GET /pic/:id');
 
-
+    return picCtrlr.fetchPic(req.user, req.params.id)
+    .then(data => res.json(data))
+    .catch(err => res.status(err.status).send(err.message));
   });
+
+  router.delete('/pic/:id', bearerAuth, (req, res) => {
+    debug('#DELETE /pic/:id');
+
+    return picCtrlr.deletePic(req.user, req.params.id)
+    .then(() => res.sendStatus(204))
+    .catch(err => res.status(err.status).send(err.message));
+  });
+  
   return router;
 };
